b9736d34c80dff1bfa947a708f11ee852a3f2b1a
Jung Andy <jungj0925@gmail.com>
2022-07-28 16:11:36 -0400

    added website

--- src/Components/CardList.js
+++ src/Components/CardList.js
@@ -4,7 +4,7 @@ import Card from './Card';
 
 const CardList = ({ robots }) => {
     const cardArray = robots.map((user, i) => {
-        return <Card key={i} id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
+        return <Card key={i} id={robots[i].id} name={robots[i].name} email={robots[i].email} website={robots[i].website}/>
     })
     return (
         <div>
